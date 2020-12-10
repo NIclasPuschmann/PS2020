e7cf3cb946f3a48574efe699bebf0ba13c58a5dd
Yewgenij Baburkin <salzguerkchen@googlemail.com>
2020-12-10 15:14:16 +0100

    create opcua sample server

---
+++ OPCUASampleServer/server.js
@@ -0,0 +1,114 @@
+"use strict";
+const opcua = require("node-opcua");
+
+
+const server = new opcua.OPCUAServer({
+    port: 19090,        // the port of the listening socket of the server
+    buildInfo: {
+        productName: "Sample NodeOPCUA Server1",
+
+        buildNumber: "7658",
+        buildDate: new Date(2020, 12, 9)
+    },
+
+    resourcePath: "/UA/MyLittleServer", //path to be added to endpoint resource name
+
+});
+
+function post_initialize(){
+    console.log("initialized");
+    function constructAddressSpace(server) {
+
+        const addressSpace = server.engine.addressSpace;
+        const namespace = addressSpace.getOwnNamespace();
+
+        // we create a new folder under RootFolder
+        const myDevice = namespace.addObject({
+            organizedBy: addressSpace.rootFolder.objects,
+            browseName: "MyDevice"
+        })
+
+        // now let's add first variable in folder
+        // the addVariableInFolder
+        let variable1 = 10.0;
+
+        //emulate variable1 changing every 500ms
+        //setInterval(function(){variable+=1},500);
+
+        server.nodeVariable1 = namespace.addVariable({
+            componentOf: myDevice,
+            nodeId: "s=Temperature",
+            browseName: "Temperature",
+            dataType: "Double",
+            value: {
+                get: () => {
+                    const t = new Date() / 10000.0;
+                    const value = variable1 + 10.0 * Math.sin(t);
+                    return new opcua.Variant({ dataType: opcua.DataType.Double, value: value });
+                }
+            }
+        });
+
+        server.nodeVariable2 = namespace.addVariable({
+            componentOf: myDevice,
+            browseName: "Capacity",
+            dataType: "Double",
+            value: {
+                get: function () {
+                    const t = new Date() / 10000.0;
+                    const value = 0.5* Math.sin(t);
+                    return new opcua.Variant({dataType: opcua.DataType.Double, value: value });
+                },
+                set: function (variant) {
+                    variable2 = parseFloat(variant.value);
+                    return opcua.StatusCodes.Good;
+                }
+            }
+        });
+
+    /*  better check out what the access levels are even for 
+
+        const nodeVariable3 = namespace.addVariable({
+            componentOf: myDevice,
+            browseName: "MyVariable3",
+            dataType: "Double",
+            arrayDimensions: [3],
+            accessLevel: "CurrentRead | CurrentWrite",
+            userAccessLevel: "CurrentRead | CurrentWrite",
+            valueRank: 1
+
+        });
+        nodeVariable3.setValueFromSource({
+            dataType: DataType.Double,
+            arrayType: VariantArrayType.Array,
+            value: [1.0, 2.0, 3.0]
+        });
+    */
+        const os = require("os");
+        const nodeVariable4 = namespace.addVariable({
+            componentOf: myDevice,
+            nodeId: "s=memory_used",
+            browseName: "Percentage Memory Used",
+            dataType: "Double",
+            minimumSamplingInterval: 1000,
+            value: {
+                get: () => {
+                    // const value = process.memoryUsage().heapUsed / 1000000;
+                    const percentageMemUsed = 1.0 - (os.freemem() / os.totalmem());
+                    const value = percentageMemUsed * 100;
+                    return new opcua.Variant({ dataType: opcua.DataType.Double, value: value });
+                }
+            }
+        });
+
+        // please 
+    }
+    constructAddressSpace(server);
+    server.start(function() {
+        console.log("Server is now listening ... ( press CTRL+C to stop)");
+        console.log("port ", server.endpoints[0].port);
+        const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+        console.log(" the primary server endpoint url is ", endpointUrl );
+    });
+}
+server.initialize(post_initialize);
